ebe6ce999598137e9d8c05379849e73dc7a9a2ac
UlianaSilchyk <ulianasi280@gmail.com>
2023-06-02 23:08:03 +0300

    creat function

--- My_first_project/My_first_project/Source.cpp
+++ My_first_project/My_first_project/Source.cpp
@@ -3,11 +3,22 @@
 #include <Windows.h>
 using namespace std;
 
+int Cube(int x)
+{
+	return x * x * x;
+}
+
 
 int main()
 {
 	SetConsoleCP(1251);
 	SetConsoleOutputCP(1251);
+
+	int x;
+	cout << "¬вед≥ть число. €ке необх≥дно п≥днести до кубу" << endl;
+	cin >> x;
+	cout << endl;
+	Cube(x);
 
 
 	return 0;
